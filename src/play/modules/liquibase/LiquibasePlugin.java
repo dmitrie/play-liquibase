ac36ed30e507df14b566d68f2896b1176248f137
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-07-07 19:01:28 +0300

    split big onApplicationStart() method to smaller methods

--- src/play/modules/liquibase/LiquibasePlugin.java
+++ src/play/modules/liquibase/LiquibasePlugin.java
@@ -22,6 +22,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static java.lang.Boolean.parseBoolean;
 
 public class LiquibasePlugin extends PlayPlugin {
 
@@ -29,145 +32,150 @@ public class LiquibasePlugin extends PlayPlugin {
 
   @Override
   public void onApplicationStart() {
-
     String autoUpdate = Play.configuration.getProperty("liquibase.active", "false");
-    String changeLogPath = Play.configuration.getProperty("liquibase.changelog", "mainchangelog.xml");
-    String propertiesPath = Play.configuration.getProperty("liquibase.properties", "liquibase.properties");
-    String scanner = Play.configuration.getProperty("liquibase.scanner", "jar");
-    String contexts = Play.configuration.getProperty("liquibase.contexts", null);
-    contexts = (null != contexts && !contexts.trim().isEmpty()) ? contexts : null;
-    String actions = Play.configuration.getProperty("liquibase.actions");
 
-    if (null == actions) {
-      throw new LiquibaseUpdateException("No valid action found for liquibase operation");
+    if (!parseBoolean(autoUpdate)) {
+      logger.info("Auto update flag [{}] != true  => skipping structural update", autoUpdate);
+      return;
     }
 
-    ResourceAccessor accessor = null;
-    if ("jar".equals(scanner)) {
-      accessor = new DuplicatesIgnorantResourceAccessor(Play.classloader);
-    }
-    else if ("src".equals(scanner)) {
-      accessor = new FileSystemResourceAccessor(Play.applicationPath.getAbsolutePath());
-    }
-    else {
-      throw new LiquibaseUpdateException("No valid scanner found liquibase operation " + scanner);
-    }
+    logger.info("Auto update flag found and positive => let's get on with changelog update");
 
-    List<LiquibaseAction> acts = new ArrayList<LiquibaseAction>();
-
-    for (String action : actions.split(",")) {
-      LiquibaseAction op = LiquibaseAction.valueOf(action.toUpperCase());
-      acts.add(op);
-    }
-
-    Database db = null;
-
-    if (true == Boolean.valueOf(autoUpdate)) {
-
-      logger.info("Auto update flag found and positive => let's get on with changelog update");
-      InputStream pstream = null;
-      InputStream clstream = null;
+    try (Connection cnx = getConnection()) {
+      Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(cnx));
 
       try {
+        Liquibase liquibase = createLiquibase(database);
+        String contexts = parseContexts();
 
-        Connection cnx = getConnection();
-
-        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(cnx));
-
-        final Liquibase liquibase = new Liquibase(changeLogPath, accessor, database);
-        if ("jar".equals(scanner)) {
-          pstream = Play.classloader.getResourceAsStream(propertiesPath);
+        for (LiquibaseAction op : parseLiquibaseActions()) {
+          performAction(liquibase, op, contexts);
         }
-        else {
-          pstream = new FileInputStream(Play.getFile(propertiesPath));
-        }
-
-        if (null != pstream) {
-          Properties props = new Properties();
-          props.load(pstream);
-
-          for (String key : props.keySet()) {
-            String val = props.get(key);
-            logger.info("found parameter [{}] / [{}] for liquibase update", key, val);
-            liquibase.setChangeLogParameter(key, val);
-          }
-        }
-        else {
-          logger.info("Could not find properties file [{}]", propertiesPath);
-        }
-
-        db = liquibase.getDatabase();
-        for (LiquibaseAction op : acts) {
-          logger.info("Dealing with op [{}]", op);
-
-          switch (op) {
-            case LISTLOCKS:
-              liquibase.reportLocks(System.out);
-              break;
-            case RELEASELOCKS:
-              liquibase.forceReleaseLocks();
-              break;
-            case SYNC:
-              liquibase.changeLogSync(contexts);
-              break;
-            case STATUS:
-              File tmp = Play.tmpDir.createTempFile("liquibase", ".status");
-              liquibase.reportStatus(true, contexts, new FileWriter(tmp));
-              logger.info("status dumped into file [{}]", tmp);
-              break;
-            case UPDATE:
-              liquibase.update(contexts);
-              break;
-            case CLEARCHECKSUMS:
-              liquibase.clearCheckSums();
-              break;
-            case VALIDATE:
-              try {
-                liquibase.validate();
-              }
-              catch (ValidationFailedException e) {
-                logger.error("liquibase validation error", e);
-              }
-            default:
-              break;
-          }
-          logger.info("op [{}] performed", op);
-        }
-      }
-      catch (SQLException | LiquibaseException | IOException sqe) {
-        throw new LiquibaseUpdateException(sqe.getMessage(), sqe);
       }
       finally {
-        if (null != db) {
-          try {
-            db.close();
-          }
-          catch (DatabaseException | JDBCException e) {
-            logger.warn("problem closing connection: " + e, e);
-          }
+        close(database);
+      }
+    }
+    catch (SQLException | LiquibaseException | IOException sqe) {
+      throw new LiquibaseUpdateException(sqe.getMessage(), sqe);
+    }
+  }
+
+  private void performAction(Liquibase liquibase, LiquibaseAction op, String contexts) throws LiquibaseException, IOException {
+    logger.info("Dealing with op [{}]", op);
+
+    switch (op) {
+      case LISTLOCKS:
+        liquibase.reportLocks(System.out);
+        break;
+      case RELEASELOCKS:
+        liquibase.forceReleaseLocks();
+        break;
+      case SYNC:
+        liquibase.changeLogSync(contexts);
+        break;
+      case STATUS:
+        File tmp = Play.tmpDir.createTempFile("liquibase", ".status");
+        liquibase.reportStatus(true, contexts, new FileWriter(tmp));
+        logger.info("status dumped into file [{}]", tmp);
+        break;
+      case UPDATE:
+        liquibase.update(contexts);
+        break;
+      case CLEARCHECKSUMS:
+        liquibase.clearCheckSums();
+        break;
+      case VALIDATE:
+        try {
+          liquibase.validate();
         }
-        if (null != pstream) {
-          try {
-            pstream.close();
-          }
-          catch (Exception e) {
-            logger.warn("problem closing pstream: " + e, e);
-          }
+        catch (ValidationFailedException e) {
+          logger.error("liquibase validation error", e);
         }
-        if (null != clstream) {
-          try {
-            clstream.close();
-          }
-          catch (Exception e) {
-            logger.warn("problem closing clstream: " + e, e);
-          }
+        break;
+    }
+    logger.info("op [{}] performed", op);
+  }
+
+  private Liquibase createLiquibase(Database database) throws LiquibaseException, IOException {
+    ResourceAccessor accessor = createResourceAccessor();
+
+    String changeLogPath = Play.configuration.getProperty("liquibase.changelog", "mainchangelog.xml");
+    Liquibase liquibase = new Liquibase(changeLogPath, accessor, database);
+
+    configureLiquibaseProperties(liquibase);
+    return liquibase;
+  }
+
+  private void close(Database database) {
+    try {
+      database.close();
+    }
+    catch (DatabaseException | RuntimeException e) {
+      logger.warn("problem closing connection: " + e, e);
+    }
+  }
+
+  String parseContexts() {
+    String contexts = Play.configuration.getProperty("liquibase.contexts", "").trim();
+    return contexts.isEmpty() ? null : contexts;
+  }
+
+  private void configureLiquibaseProperties(Liquibase liquibase) throws IOException {
+    String propertiesPath = Play.configuration.getProperty("liquibase.properties", "liquibase.properties");
+    try (InputStream in = readPropertiesFile(propertiesPath)) {
+      if (in != null) {
+        Properties props = new Properties();
+        props.load(in);
+
+        for (Map.Entry<String, String> stringStringEntry : props.entrySet()) {
+          String val = stringStringEntry.getValue();
+          logger.info("found parameter [{}] / [{}] for liquibase update", stringStringEntry.getKey(), val);
+          liquibase.setChangeLogParameter(stringStringEntry.getKey(), val);
         }
       }
+      else {
+        logger.info("Could not find properties file [{}]", propertiesPath);
+      }
+    }
+  }
 
+  private InputStream readPropertiesFile(String propertiesPath) throws FileNotFoundException {
+    String scanner = Play.configuration.getProperty("liquibase.scanner", "jar");
+    switch (scanner) {
+      case "jar":
+        return Play.classloader.getResourceAsStream(propertiesPath);
+      default:
+        return new FileInputStream(Play.getFile(propertiesPath));
     }
-    else {
-      logger.info("Auto update flag [{}] != true  => skipping structural update", autoUpdate);
+  }
+
+  private ResourceAccessor createResourceAccessor() {
+    String scanner = Play.configuration.getProperty("liquibase.scanner", "jar");
+
+    switch (scanner) {
+      case "jar":
+        return new DuplicatesIgnorantResourceAccessor(Play.classloader);
+      case "src":
+        return new FileSystemResourceAccessor(Play.applicationPath.getAbsolutePath());
+      default:
+        throw new LiquibaseUpdateException("No valid scanner found liquibase operation " + scanner);
     }
+  }
+
+  List<LiquibaseAction> parseLiquibaseActions() {
+    String liquibaseActions = Play.configuration.getProperty("liquibase.actions", "");
+    if (liquibaseActions == null || liquibaseActions.isEmpty()) {
+      throw new LiquibaseUpdateException("No valid action found for liquibase operation. Please set property 'liquibase.actions'.");
+    }
+
+    List<LiquibaseAction> actions = new ArrayList<>();
+    for (String action : liquibaseActions.split(",")) {
+      LiquibaseAction op = LiquibaseAction.valueOf(action.toUpperCase());
+      actions.add(op);
+    }
+
+    return actions;
   }
 
   @SuppressWarnings("CallToDriverManagerGetConnection")
