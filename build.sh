6cab13d7663ea5cac87d7cd74ebc84ce46bb366d
Dmitri Ess <dmitri.ess@codeborne.com>
2017-12-19 00:51:57 +0200

    build using travis and upload tagged builds to github

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-ORGANIZATION="play-liquibase"
-MODULE="liquibase"
-VERSION=`grep self conf/dependencies.yml | sed "s/.*$MODULE //"`
-TARGET=/var/www/repo/$ORGANIZATION/$MODULE-$VERSION.zip
 
 rm -fr dist
 play dependencies --sync || exit $?
 play build-module || exit $?
-
-if [ -e $TARGET ]; then
-    echo "Not publishing, $MODULE-$VERSION already exists"
-else
-    cp dist/*.zip $TARGET
-fi
